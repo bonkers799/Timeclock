c3124c4d09bbb30411e3332f6db4126f7ea3eae9
bonkers799 <bwest799799@gmail.com>
2024-02-04 21:44:33 -0500

    Beta
    Added additional error handling as well as allowed for additional functionality like clocking in and out while changing the database, built out the create and delete button, and started building out the weekly reports

--- TimeclockApp/UserPage.py
+++ TimeclockApp/UserPage.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 import mysql.connector
 from tkinter import ttk
+from datetime import datetime
 
 class UserWindow:
-    def __init__(self, master):
-        self.master = master
-        self.user_page()
+    def __init__(self, root):
+        self.root = root
+        self.user_page
 
-    def user_page(self):
+    def user_page(self, pin):
         self.dbConnection = mysql.connector.connect(
             host="localhost",
             user="root",
@@ -15,25 +16,54 @@ class UserWindow:
             database="timeclockdb"
         )
 
-        for widget in self.master.winfo_children():
+        self.cursor = self.dbConnection.cursor()
+        
+
+        for widget in self.root.winfo_children():
             widget.destroy()
 
-        label1 = tk.Label(self.master, text="Employee Punch Clock", font=("Cooper Black", 36), justify="center")
+        label1 = tk.Label(self.root, text="Employee Punch Clock", font=("Cooper Black", 36), justify="center")
         label1.pack(padx=20, pady=100)
 
-        btnframe = tk.Frame(self.master, width=150, height=150)
+        btnframe = tk.Frame(self.root, width=150, height=150)
         btnframe.columnconfigure(0, weight=1)
         btnframe.columnconfigure(1, weight=1)
         btnframe.pack(padx=20, pady=100, ipadx=150, ipady=250)
 
-        clockIn = ttk.Button(btnframe, text="Clock In", command=self.clock_in)
+        clockIn = ttk.Button(btnframe, text="Clock In", command=lambda: self.clock_in(pin))
         clockIn.grid(row=0, column=0, sticky=tk.NSEW, padx=50)
 
-        clockOut = ttk.Button(btnframe, text="Clock Out", command=self.clock_out)
+        clockOut = ttk.Button(btnframe, text="Clock Out", command=lambda: self.clock_out(pin))
         clockOut.grid(row=0, column=1, sticky=tk.NSEW, padx=50)
     
-    def clock_in(self):
-        print("clock in")
+    def clock_in(self, pin):
+        d = datetime.today()
+        hr = datetime.now().time()
+        min = datetime.now().time()
+        sec = datetime.now().time()
 
-    def clock_out(self):
-        print("clock out")
+        pinTemp = str(pin)
+        pinFinal = pinTemp[1:5]
+
+        date = d.strftime("%m/%d/%y")
+        clockIn = hr.strftime("%H") + ":" + min.strftime("%M") + ":" + sec.strftime("%S")
+        query = "INSERT INTO time (date, clock_in) VALUES (" + date + ", " + clockIn + ") WHERE employee_pin=" + pinFinal
+
+        print("SQL Query: " + query)
+        #self.cursor.execute(query)
+
+    def clock_out(self, pin):
+        d = datetime.today()
+        hr = datetime.now().time()
+        min = datetime.now().time()
+        sec = datetime.now().time()
+
+        pinTemp = str(pin)
+        pinFinal = pinTemp[1:5]
+
+        date = d.strftime("%m/%d/%y")
+        clockOut = hr.strftime("%H") + ":" + min.strftime("%M") + ":" + sec.strftime("%S")
+        query = "INSERT INTO time (date, clock_out) VALUES (" + date + ", " + clockOut + ") WHERE employee_pin=" + pinFinal
+
+        print("SQL Query: " + query)
+        #self.cursor.execute(query)
